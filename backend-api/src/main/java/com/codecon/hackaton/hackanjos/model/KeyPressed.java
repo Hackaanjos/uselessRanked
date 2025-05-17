3c958fd7db19fdc614af34c5e5c7d0f41b612bc0
Jeliel <jelielbraz71@gmail.com>
2025-05-17 16:03:58 -0300

    Adicionar entidade de keyPressed

--- backend-api/src/main/java/com/codecon/hackaton/hackanjos/model/KeyPressed.java
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/model/KeyPressed.java
@@ -1,4 +1,35 @@
 package com.codecon.hackaton.hackanjos.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Getter
+@Setter
+@Table
 public class KeyPressed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Size(min=1, max=1)
+    @Column(nullable = false)
+    String keyCode;
+
+    @Column(nullable = false)
+    Long eventCounter;
+
+    @Column(nullable = false)
+    LocalDateTime eventDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
 }

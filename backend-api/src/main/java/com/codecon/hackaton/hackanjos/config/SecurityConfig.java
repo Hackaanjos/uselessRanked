aa8818d7f103b3ceab5e1f9a5eb42b3c8b5d187c
Bruno Miguel Morais <bruno.morais@asaas.com.br>
2025-05-17 14:57:30 -0300

    Update backend-api/src/main/java/com/codecon/hackaton/hackanjos/config/SecurityConfig.java
    Co-authored-by: Jeliel Braz <jelielbraz71@gmail.com>

--- backend-api/src/main/java/com/codecon/hackaton/hackanjos/config/SecurityConfig.java
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+        
         return source;
     }
 }

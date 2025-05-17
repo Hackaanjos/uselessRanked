16ad5c48ecdc1381516c0a74aaa1b4de29fc7b77
Jeliel <jelielbraz71@gmail.com>
2025-05-17 10:36:22 -0300

    start be

---
+++ backend-api/src/test/java/com/codecon/hackaton/hackanjos/HackanjosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecon.hackaton.hackanjos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackanjosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a9912397d7c435e7c2c6ec605d01e4cd650c6429
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2025-05-17 20:59:28 -0300

    upd: Finalizando PaginatedList

--- site/src/app/models/PaginatedList.ts
+++ site/src/app/models/PaginatedList.ts
@@ -1,27 +1,27 @@
-interface Sort {
+export interface Sort {
     sorted: boolean;
     empty: boolean;
     unsorted: boolean;
 }
 
 export interface Pageable {
-    pageNumber: number,
-    pageSize: number,
-    sort: Sort
-    offset: number,
-    paged: boolean,
-    unpaged: boolean
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
 }
 
 export interface PaginatedList<T> {
-    content: Array[T];
-    last: boolean,
-    totalPages: number,
-    totalElements: number,
-    first: boolean,
-    size: number,
-    number: number,
-    sort: Sort,
-    numberOfElements: 3,
-    empty: false
+    content: Array<T>;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: number;
+    empty: boolean;
 }

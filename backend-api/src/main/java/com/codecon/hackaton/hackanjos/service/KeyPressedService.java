56cee4a233311362a1db3db7857af8b53b0aba49
Jeliel <jelielbraz71@gmail.com>
2025-05-17 16:05:10 -0300

    Adicionar service com logica de cadastro de keypressed

---
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/service/KeyPressedService.java
@@ -0,0 +1,39 @@
+package com.codecon.hackaton.hackanjos.service;
+
+import com.codecon.hackaton.hackanjos.model.KeyPressed;
+import com.codecon.hackaton.hackanjos.model.User;
+import com.codecon.hackaton.hackanjos.repository.KeyPressedRepository;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class KeyPressedService {
+
+    private KeyPressedRepository keyPressedRepository;
+
+    public void saveOrUpdateEvent(String keyCode, Long eventCounter, User user) {
+        LocalDateTime localStartDateTime = LocalDate.now().atStartOfDay();
+        LocalDateTime localEndDateTime = LocalDate.now().atTime(23, 59, 59);
+        KeyPressed keyPressed = keyPressedRepository.getKeyPressedByKeyCodeAndUserIdAndEventDateBetween(keyCode, user.getId(), localStartDateTime, localEndDateTime);
+
+        if (keyPressed == null) {
+            keyPressed = new KeyPressed();
+            keyPressed.setKeyCode(keyCode);
+            keyPressed.setEventCounter(eventCounter);
+            keyPressed.setEventDate(LocalDateTime.now());
+            keyPressed.setUser(user);
+            keyPressedRepository.save(keyPressed);
+        } else {
+            keyPressed.setEventCounter(keyPressed.getEventCounter() + eventCounter);
+            keyPressed.setEventDate(LocalDateTime.now());
+            keyPressedRepository.save(keyPressed);
+        }
+    }
+}

088e303aa66759c93acb2b8ef4bfcdbba1ed9a60
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2025-05-17 23:00:43 -0300

    fix: Corrigindo consulta de dados da API, passando filtro de data

--- site/src/app/services/ranking.service.interface.ts
+++ site/src/app/services/ranking.service.interface.ts
@@ -1,8 +1,14 @@
 import { Observable } from 'rxjs';
 import { PaginatedList } from "../models/PaginatedList";
 import { UserRanking } from "../models/UserRanking";
+import { PeriodType } from '../../utils/enums/PeriodType';
 
 export interface RankingServiceInterface {
-  getSingleKeyRanking(key: string): Observable<PaginatedList<UserRanking>>;
-  getAllKeysRanking(): Observable<PaginatedList<UserRanking>>;
+
+  getSingleKeyRanking(key: string, periodType: PeriodType): Observable<PaginatedList<UserRanking>>;
+  listSingleKeyRankings(key: string): Record<PeriodType, UserRanking[]>;
+
+  getAllKeysRanking(periodType: PeriodType): Observable<PaginatedList<UserRanking>>;
+  listAllKeysRanking(): Record<PeriodType, UserRanking[]>;
+
 }

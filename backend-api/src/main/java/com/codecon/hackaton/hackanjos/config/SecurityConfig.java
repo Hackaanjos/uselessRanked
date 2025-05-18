9a12f06d7df90c5df0d8fd027f79c335d873f38f
Jeliel <jelielbraz71@gmail.com>
2025-05-17 22:35:06 -0300

    Deixando privado os metodos de save

--- backend-api/src/main/java/com/codecon/hackaton/hackanjos/config/SecurityConfig.java
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .cors().and()
             .csrf().disable()
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/api/auth/**", "/oauth2/**", "/login/**", "/swagger-ui/**", "/api-docs/**", "/api/keypressed/**", "/api/mouseclick/**", "/api/ranking/**").permitAll()
+                .requestMatchers("/", "/api/auth/**", "/oauth2/**", "/login/**", "/swagger-ui/**", "/api-docs/**", "/api/ranking/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2

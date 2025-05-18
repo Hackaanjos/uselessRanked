2a96263e597c237cbf61032f30def4e88f341034
Bruno Miguel Morais <bruno.morais@asaas.com.br>
2025-05-18 02:20:38 -0300

    Ajustando nome

--- backend-api/src/main/java/com/codecon/hackaton/hackanjos/model/enums/AchievementName.java
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/model/enums/AchievementName.java
@@ -12,7 +12,7 @@ public enum AchievementName {
     MISS_CLICK("Miss click?", "Clicou pela primeira vez", 1, "Mouse"),
     CUNEIFORM("Escrita cuneiforme", "Os livros de história dizem que os Sumérios inventaram a escrita, mas nós sabemos que foi você!", 1, "Mouse"),
     XIQUE_XIQUE("Xique-xique, Bahia", "Você mexeu tanto o mouse, que chegou em Xique-Xique, Bahia", 1494000, "Mouse"),
-    EIFEEL_TOWER("Mais alto que a Torre Eiffel", "Você scrollou tanto que chegou no topo da Torre Eiffel", 312, "Mouse"),
+    EIFFEL_TOWER("Mais alto que a Torre Eiffel", "Você scrollou tanto que chegou no topo da Torre Eiffel", 312, "Mouse"),
     HOLY_BIBLE("A Bíblia Sagrada", "Já digitou mais letras do que tem na Bíblia", 400000, "Teclado");
 
     private String name;

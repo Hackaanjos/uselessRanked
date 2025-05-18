7f3ec0f205c5846988d026f39a43e5957544bae0
Jeliel <jelielbraz71@gmail.com>
2025-05-17 22:25:42 -0300

    Add AllAgrs

--- backend-api/src/main/java/com/codecon/hackaton/hackanjos/dto/reponse/ranking/MouseClickDTO.java
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/dto/reponse/ranking/MouseClickDTO.java
@@ -1,10 +1,12 @@
 package com.codecon.hackaton.hackanjos.dto.reponse.ranking;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class MouseClickDTO {
 
     Long eventCounter;

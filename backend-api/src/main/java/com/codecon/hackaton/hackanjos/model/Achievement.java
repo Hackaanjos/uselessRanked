4f3324e4b79d9453d2e3a930897efca90e04ce80
Bruno Miguel Morais <bruno.morais@asaas.com.br>
2025-05-18 01:20:18 -0300

    Adicionando achievements

---
+++ backend-api/src/main/java/com/codecon/hackaton/hackanjos/model/Achievement.java
@@ -0,0 +1,29 @@
+package com.codecon.hackaton.hackanjos.model;
+
+import com.codecon.hackaton.hackanjos.model.enums.AchievementName;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table
+public class Achievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    AchievementName name;
+
+    @Column(nullable = false)
+    LocalDateTime achievedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
